7cbbb82132859bf15bc7fd422dd7ebcfc8242541
Yusley <5003489915@student.csn.edu>
2019-10-15 13:29:48 -0700

    Added intent

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -46,6 +46,12 @@ class GameWonFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.winner_menu, menu)
+
+        //Check if the activity resolves
+        if(null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
+
     }
 
     private fun getShareIntent(): Intent {
